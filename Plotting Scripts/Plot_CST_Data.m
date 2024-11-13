7f9da167c5e7fb1382e95995e987aa501bdf0e06
alistairmuir <alistair.muir@uni-rostock.de>
2024-11-13 12:52:03 +0100

    Adding script that plots real/complex CST data with no augmentation.

---
+++ Plotting Scripts/Plot_CST_Data.m
@@ -0,0 +1,34 @@
+%% Script to plot imported, untreated CST data
+clc
+clear
+
+%% USER INPUT
+fname = ["o1(3),pb.txt", "o2(3),pb.txt"] ;
+file_string = "CST Files/Pillbox/Wake_15GHz_30cpw_3modes_10sigma_1M/Export/Port signals/"+fname ;
+
+%% Import
+
+Nfiles = length(file_string) ;
+
+[x,y] = func_Import_CSTdata(file_string(1), 1) ;
+data_y = zeros([size(y), Nfiles]) ;
+
+for i=2:Nfiles
+    [x,y] = func_Import_CSTdata(file_string(i), 1) ;
+    data_y(:,:,i) = y ;
+end
+
+%% FFT
+for i=1:Nfiles
+    [freqs, y_fft(:,i)] = func_FFT_CSTdata(data_y(:,:,i),x) ;
+end
+
+%% Plot
+figure(1); clf;
+hold on
+for i=1:Nfiles
+    plot(freqs,abs(y_fft(:,i)))
+end
+set(gca, "YScale", "log")
+grid on
+grid minor
