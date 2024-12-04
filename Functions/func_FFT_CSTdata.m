f96ede2a14e990760a476033bd26b17e0438a0f3
alistairmuir <alistair.muir@uni-rostock.de>
2024-12-04 13:57:26 +0100

    Hann windowing now used, corrected so that it acts as raised-cosine filter.

--- Functions/func_FFT_CSTdata.m
+++ Functions/func_FFT_CSTdata.m
@@ -9,36 +9,35 @@ function [freqs, data_FD] = func_FFT_CSTdata(data_TD, time_samples)
 % :returns: freqs (1-by-N), data_FD (1-by-N)
 
 
-%%% Ensure timeseries has even number of samples (cuts off final odd sample).
-% Determine whether number of time samples, N_ts, is odd or even.
+% %%% Determine next power of 2 above number of samples for efficient FFT.
 N_s = length(time_samples) ;
-Nmod2 = mod(N_s, 2) ;
 N_pow2 = 2.^nextpow2(N_s) ;
 
-% Reduce N_ts by 1 if N_ts is odd (N_ts is unchanged if even).
-N_s = N_s - Nmod2 ;
-
-% Cut off final odd sample from all timeseries if present.
-time_samples = time_samples(1:N_s) ;
-data_TD      = data_TD(1:N_s,:) ;
-
+% Apply Hann window.
+hann_window = hann(N_s*2) ;
+data_TD = data_TD.*(hann_window(N_s+1:end)) ;      % Hann window (for wide main lobe)
 
 %%% Calculate sample frequencies
 % Frequency interval.
-fs = 1/((time_samples(end)-time_samples(1))/N_s) ;
+fs = 1/((time_samples(end)-time_samples(1))/N_pow2) ;
 
 % Frequencies for one-sided spectrum.
-freqs = fs/N_s*(0:N_s/2-1) ;
+freqs = fs/N_pow2*(0:N_pow2/2) ;
 
-
-%%% Carry out FFT
+%%% Carry out FFT.
 data_rawFFT = fft(data_TD, N_pow2) ;
 
 %%% Post FFT treatments to form one-sided spectra.
 % Take only positive frequencies for one-sided spectra.
-data_FD = data_rawFFT(1:N_s/2,:)/N_s ;
+data_FD = data_rawFFT(1:N_pow2/2+1,:)/N_pow2 ;
 
 % Multiply by two to account for negative frequencies (not including DC or Nyquist).
 data_FD(2:end-1) = 2*data_FD(2:end-1) ;
+
+% data_FD = fft(data_TD) ;
+% 
+% % Determine the frequencies on which the transfer functions are sampled
+% delta_T = time_samples(end) - time_samples(1) ;
+% freqs = 1/delta_T*((1:N_s) - 1) ;
 
 end
