b0533022a7cf69e06a7e78109df97b7b032efba7
alistairmuir <alistair.muir@uni-rostock.de>
2024-12-05 11:03:22 +0100

    Matrices for all segments are now loaded an stored before looping through frequencies. 100x speed improvement whoop whoop!

--- Functions/func_CalcScsc.m
+++ Functions/func_CalcScsc.m
@@ -19,22 +19,33 @@ function [S, freqs, Length] = func_CalcScsc(seg_dir, segment_names, orthogonal_m
 % Physical constants
 [~, ~, c0] = func_EM_PhysicalConstants() ;
 
-% Load first segment generalized matrix.
-seg_matrix = load(seg_dir+"/"+segment_names(1)) ;
-
-% Retrieve sample frequencies and number of samples.
-freqs = seg_matrix.f ;
-N_f = length(freqs) ;
-
 % Retrieve number of segments.
 N_segs = length(segment_names) ;
 
-% Construct array of segment lengths and populate first element.
+% Initialize array for segment lengths.
 L_segs = zeros(1,N_segs) ;
-L_segs(1) = seg_matrix.Length ;
+
+% Retrieve GM for all segments.
+seg_matrix = struct ;
+
+for segi=1:N_segs
+    seg_struct = load(seg_dir+"/"+segment_names(segi)) ;
+    
+    % Store GM in a struct array.
+    seg_matrix(segi).S = seg_struct.S ;
+    seg_matrix(segi).f = seg_struct.f ;
+    
+    % Store segment lengths in an array for easy summing.
+    L_segs(segi) = seg_struct.Length ;
+end
+
+% Retrieve sample frequencies and number of samples from last segment
+% (freqs should be the same for all segments).
+freqs = seg_matrix(1).f ;
+N_f   = length(freqs) ;
 
 % Calculate number of modes from GM of first segment.
-N_size  = length(seg_matrix.S(1,1,:)) ;
+N_size  = length(seg_matrix(1).S(1,1,:)) ;
 N_modes = (N_size-1)/2 ;
 
 % Construct path for orthogonal matrices file.
@@ -50,7 +61,6 @@ N_intmodes = length(F) ;  %   includes both ports, i.e. N_intmodes = 2*N_int, us
 % Intialize final generalized matrix for complete path.
 S = complex(zeros(N_f,N_size,N_size)) ;
 
-
 %%% Carry out CSC-beam for each sample frequency.
 for fi = 1:N_f
         
@@ -62,22 +72,16 @@ for fi = 1:N_f
     % Cycle through all segments to create block S matrix, S_tot, and arrays for length and phi.
     for segi=1:N_segs
         
-        % Load S-matrix.
-        seg_matrix = load(seg_dir+"/"+segment_names(segi)) ;
-        
         % S block diagonal matrix.
-        S_tot = blkdiag(S_tot, squeeze(seg_matrix.S(fi,:,:))) ;
-        
-        % Retrieve segment length.
-        L_segs(segi) = seg_matrix.Length ;
-        
+        S_tot = blkdiag(S_tot, squeeze(seg_matrix(segi).S(fi,:,:))) ;
+                
         % Phase at end of each segment.
-        phi_segs(segi) = func_CalcPhase(deg2rad(freqs(fi)), sum(L_segs(1:segi)), 0, c0) ;
+        phi_segs(segi) = func_CalcPhase(freqs(fi), sum(L_segs(1:segi)), 0, c0) ;
         
     end
         
-    % Create matrix for applying phase adjustment (ignoring downstream ext. port)
-    M = func_ConstructM(phi_segs, N_modes) ;
+    % Create matrix for applying phase adjustment at each port 1.
+    M = func_ConstructM(phi_segs(1:end-1), N_modes) ;
     
     % Reorder block matrix according to internal and external quantities
     % and according to current and voltages.
