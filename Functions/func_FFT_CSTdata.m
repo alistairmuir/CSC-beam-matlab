e5d2ead2ecded4980b28dc77ffeb71fd678ae8ef
alistairmuir <alistair.muir@uni-rostock.de>
2024-12-05 22:16:54 +0100

    Removing post-FFT treatment, since it doesn't affect the transfer function (e.g. FFT(signal)/FFT(current))

--- Functions/func_FFT_CSTdata.m
+++ Functions/func_FFT_CSTdata.m
@@ -13,26 +13,19 @@ function [freqs, data_FD] = func_FFT_CSTdata(data_TD, time_samples)
 N_s = length(time_samples) ;
 N_pow2 = 2^nextpow2(N_s) ;
 
-% Apply Hann window.
-hann_window = hann(N_s*2) ;
+%%% Apply Hann window.
+hann_window = hann(2*N_s) ;
 data_TD = data_TD.*(hann_window(N_s+1:end)) ;
         % Latter half of Hann window = raised cosine with zero at final data point.
 
 %%% Calculate sample frequencies
-% Frequency interval.
-fs = 1/((time_samples(end)-time_samples(1))/N_s) ;
+% Sample period
+sample_freq = N_pow2/(time_samples(end)-time_samples(1)) ;
 
 % Frequencies for one-sided spectrum.
-freqs = fs/N_pow2*(0:N_pow2/2) ;
+freqs = sample_freq/N_pow2*(0:N_pow2-1) ;
 
 %%% Carry out FFT.
-data_rawFFT = fft(data_TD, N_pow2) ;
-
-%%% Post-FFT treatments to form one-sided spectra.
-% Take only positive frequencies for one-sided spectra.
-data_FD = data_rawFFT(1:N_pow2/2+1,:)/N_pow2 ;
-
-% Multiply by two to account for negative frequencies (not including DC or Nyquist).
-data_FD(2:end-1,:) = 2*data_FD(2:end-1,:) ;
+data_FD = fft(data_TD, N_pow2, 1) ;
 
 end
