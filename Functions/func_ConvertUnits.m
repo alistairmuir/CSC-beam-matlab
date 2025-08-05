7ae66fcfbd960dba3b8a05915638ecc15ef5b609
alistair <alistair@139.30.92.168>
2025-08-05 14:10:05 +0200

    Comments added.

--- Functions/func_ConvertUnits.m
+++ Functions/func_ConvertUnits.m
@@ -1,7 +1,12 @@
 function data_converted = func_ConvertUnits(data_input, conversion_factor)
 % Function to convert data into different units, with the input conversion factor.
 %
-% returns: Converted data in same format as input.
+% :param data_input: N-dim array of arbitrary data
+% :type  data_input: double
+% :param conversion_factor: Single value or vector to multiply the data.
+% :type  conversion_factor: double
+%
+% :returns: data_converted (data array in same format as data_input)
 
 data_converted = data_input .* conversion_factor ;
 
