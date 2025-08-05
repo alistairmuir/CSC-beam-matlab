9ec8ef0f58b3a3e6b029fa5bb1d594c37935b1a1
alistair <alistair@139.30.92.168>
2025-08-05 14:06:02 +0200

    Comments added.

--- Functions/func_CSTfilenames_Freq.m
+++ Functions/func_CSTfilenames_Freq.m
@@ -1,8 +1,17 @@
 function [S_dir, E1_dir, E2_dir] = func_CSTfilenames_Freq(freq_dir, freq1, Pmodes)
+% Function that outputs lists of directories for CST results in the CST Navigation
+% tree folder structure.
+%
+% :param freq_dir: Directory containing the CST simulation results in frequency domain.
+% :type  freq_dir: string
+% :param freq1: First frequency where electric field was monitored (CST uses this frequency
+%               for its filename convention.
+% :type  freq1: double
+% :param Pmodes: List of modes to be included in generalized matrix.
+% :type  Pmodes: integer
 % 
 % 
-% 
-% returns: strings for filenames of wake_Z, current, o1, o2
+% :returns: S_dir, E1_dir (Nmodes-by-1), E2_dir (Nmodes-by-1)
 
 % S-parameters
 S_dir = freq_dir+"/S-Parameters/" ;
