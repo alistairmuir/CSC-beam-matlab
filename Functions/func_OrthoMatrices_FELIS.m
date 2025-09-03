fa47e125d3ea5febd32e7c4166b4333b89f72087
alistair <alistair@139.30.92.168>
2025-09-03 10:27:25 +0200

    Commenting and variable name updates for clarity.

--- Functions/func_OrthoMatrices_FELIS.m
+++ Functions/func_OrthoMatrices_FELIS.m
@@ -1,25 +1,41 @@
 function [P, F] = func_OrthoMatrices_FELIS(N_segs, nTE1, nTM1, nTE2, nTM2)
+% Function to create and output orthogonal matrices P and F, as described in [1], 
+% for a CSC-beam problem with the input number of segments and modes.
 %
+% References:
+% 1. "Generalization of coupled S-parameter calculation to compute beam
+% impedances in particle accelerators" - T. Flisgen, E. Gjonaj, H.W. Glock - 2020
 %
-%
-% :returns: P (N-by-N), F (N_int-by-N_int)
+% :param N_segs: Number of segments to be concatenated by CSC-beam.
+% :type  N_segs: integer
+% :param nTE1: List of number of TE modes for port 1 of all segments.
+% :type  nTE1: integer
+% :param nTM1: List of number of TM modes for port 1 of all segments.
+% :type  nTM1: integer
+% :param nTE2: List of number of TE modes for port 2 of all segments.
+% :type  nTE2: integer
+% :param nTM2: List of number of TM modes for port 2 of all segments.
+% :type  nTM2: integer
+% :returns: P (N-by-N), F (M-by-M) where M = number of internal port modes and N = total number of all modes + N_segs
 
-%%% Permutation matrix, P %%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Consistency check.
-for i=1:N_segs-1
-    if ((nTE2(i) ~= nTE1(i+1)) || (nTM2(i) ~= nTM1(i+1)))
+for ii=1:N_segs-1
+    if ((nTE2(ii) ~= nTE1(ii+1)) || (nTM2(ii) ~= nTM1(ii+1)))
         error('inconsistent port modes!');
     end
 end
 
+
+%%% Permutation matrix, P %%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Calc total number of modes for each port
 nMode1 = zeros(N_segs,1);
 nMode2 = zeros(N_segs,1);
-for i=1:N_segs
-    nMode1(i)=nTE1(i)+nTM1(i);
-    nMode2(i)=nTE2(i)+nTM2(i);
+for ii=1:N_segs
+    nMode1(ii)=nTE1(ii)+nTM1(ii);
+    nMode2(ii)=nTE2(ii)+nTM2(ii);
 end
 
 % Initialize permutation matrix
@@ -30,50 +46,50 @@ P = zeros(dim,dim);
 % for every cell there are 3 blocks of colums (Port1,Port2,Beam). Total number of
 % such blocks is 3*Ncell. Store dimensions of each block in @list.
 list=[];
-for i=1:N_segs
-    list = [list nMode1(i), nMode2(i), 1];
+for ii=1:N_segs
+    list = [list nMode1(ii), nMode2(ii), 1];
 end
 
 
 %%% INTERNAL PORTS
 row_index1 = 0;
 
-for i = 1:N_segs
-    if (i==1) % First cell: port2 block index 
-        blck_index=(i-1)*3+2; 
+for ii = 1:N_segs
+    if (ii==1) % First cell: port2 block index 
+        blck_index=(ii-1)*3+2; 
         col_index1=sum(list(1:(blck_index-1)))+1;
         col_index2=sum(list(1:(blck_index))); 
-        row_index2=row_index1+nMode2(i);
+        row_index2=row_index1+nMode2(ii);
         row_index1=row_index1+1;     
-        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode2(i));
+        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode2(ii));
         row_index1 = row_index2;
 
-    elseif (i==N_segs) % Last cell: port1 block index 
-        blck_index=(i-1)*3+1;
+    elseif (ii==N_segs) % Last cell: port1 block index 
+        blck_index=(ii-1)*3+1;
         col_index1=sum(list(1:(blck_index-1)))+1;
         col_index2=sum(list(1:(blck_index)));
-        row_index2=row_index1+nMode1(i);
+        row_index2=row_index1+nMode1(ii);
         row_index1=row_index1+1;     
-        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode1(i));
+        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode1(ii));
         row_index1 = row_index2;
 
     else
         % Internal cell: port1 block index 
-        blck_index=(i-1)*3+1; 
+        blck_index=(ii-1)*3+1; 
         col_index1=sum(list(1:(blck_index-1)))+1;
         col_index2=sum(list(1:(blck_index)));    
-        row_index2=row_index1+nMode1(i);
+        row_index2=row_index1+nMode1(ii);
         row_index1=row_index1+1;
-        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode1(i));   
+        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode1(ii));   
         row_index1 = row_index2;
         
         % Internal cell: port2 block index 
-        blck_index=(i-1)*3+2; 
+        blck_index=(ii-1)*3+2; 
         col_index1=sum(list(1:(blck_index-1)))+1;
         col_index2=sum(list(1:(blck_index)));
-        row_index2=row_index1+nMode2(i);
+        row_index2=row_index1+nMode2(ii);
         row_index1=row_index1+1;
-        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode2(i));
+        P(row_index1:row_index2,col_index1:col_index2) = eye(nMode2(ii));
         row_index1 = row_index2;
     end
 end
@@ -101,8 +117,8 @@ P(row_index1:row_index2,col_index1:col_index2) = eye(nMode2(N_segs));
 row_index1 = row_index2;
 
 %%% BEAM MODES
-for i=1:N_segs
-    blck_index=(i-1)*3+3; %beam block index
+for ii=1:N_segs
+    blck_index=(ii-1)*3+3; %beam block index
     col_index1=sum(list(1:(blck_index-1)))+1;
     col_index2=sum(list(1:(blck_index)));
     row_index1=row_index1+1;
@@ -116,9 +132,9 @@ F = [zeros(nMode2(1),nMode2(1)), eye(nMode2(1));
     eye(nMode2(1)), zeros(nMode2(1),nMode2(1))];
 
 % For each port 1-port 2 couple, repeat same feedback matrix.
-for i=2:N_segs-1
+for ii=2:N_segs-1
     F = blkdiag(F,...
-        [zeros(nMode2(i),nMode2(i)), eye(nMode2(i));
-        eye(nMode2(i)), zeros(nMode2(i),nMode2(i))]);
+        [zeros(nMode2(ii),nMode2(ii)), eye(nMode2(ii));
+        eye(nMode2(ii)), zeros(nMode2(ii),nMode2(ii))]);
 end
 end
